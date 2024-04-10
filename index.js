155f0eb706107de243e38d7b6cf286e008cbe112
Yulia Avramenko <miraclejuliet@yandex.ru>
2024-04-10 21:50:55 +0300

    Finish project

---
+++ index.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < 10; i++) {
+    const snowflake = document.createElement("div");
+    const racoon = document.createElement('video');
+    racoon.classList.add("video", "video-racoon");
+    racoon.muted = "muted";
+    racoon.autoplay = "autoplay";
+    racoon.loop = "loop";
+    racoon.src = "./assets/raccoon.mp4";
+    racoon.style.left = `${Math.random() * 100}%`;
+    racoon.style.animationDuration = `${Math.random() * 3 + 2}s`;
+    document.querySelector(".racoons").appendChild(racoon);
+
+
+}
